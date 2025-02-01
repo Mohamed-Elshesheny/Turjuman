1567756424fbb9a177c6269a4c2c4a7a1fd0404b
Mohamed-Elshesheny <mohamedelshesheny@std.mans.edu.eg>
2025-02-01 14:06:09 +0200

    Cookie

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -41,16 +41,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     isPremium: req.body.isPremium,
     role: req.body.role,
   });
-
-  const token = signToken(newUser._id);
-
-  res.status(201).json({
-    status: "success",
-    token,
-    data: {
-      user: newUser,
-    },
-  });
+  createSendToken(newUser, 201, res);
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -70,12 +61,7 @@ exports.login = catchAsync(async (req, res, next) => {
     user.isActive = true;
     await user.save({ validateModifiedOnly: true });
   }
-
-  const token = signToken(user._id);
-  res.status(200).json({
-    status: "success",
-    token,
-  });
+  createSendToken(user, 200, res);
 });
 
 exports.protect = catchAsync(async (req, res, next) => {
