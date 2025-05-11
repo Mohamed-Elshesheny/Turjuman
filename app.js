4ac7485b1dcf2ab6c789c2ec59561eec23aa6fdc
Mohamed-Zakaria <Mohamedelshesheny62@gmail.com>
2025-05-12 00:08:34 +0300

    🌐⚙️ Enable CORS with dynamic origin and credentials.

--- app.js
+++ app.js
@@ -49,6 +49,14 @@ const corsOptions = {
   allowedHeaders: ["Content-Type", "Authorization"],
 };
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
 app.use(cors(corsOptions));
 
 // 🍪 Cookie Parser
